070dfb094eb36588b6251cdec63442eea28f5700
jshph <joshua.pham@gmail.com>
2023-06-25 06:12:43 +0000

    stash changes with this being the webrtc server

--- nodes.py
+++ nodes.py
@@ -1106,18 +1106,33 @@ class LoadImage:
     #         mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
     #     return (image, mask)
     def load_image(self, image):
-        # image_path = folder_paths.get_annotated_filepath(image)
-        # i = Image.open(image_path)
-        # i = ImageOps.exif_transpose(i)
-        # image = i.convert("RGB")
-        image = np.array(json.loads(image)).astype(np.float32)
-        image = torch.from_numpy(image)[None,]
-        # if 'A' in i.getbands():
-        #     mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
-        #     mask = 1. - torch.from_numpy(mask)
-        # else:
-        mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-        return (image, mask)
+        try:
+
+            # image_path = folder_paths.get_annotated_filepath(image)
+            # i = Image.open(image_path)
+            # i = ImageOps.exif_transpose(i)
+            # image = i.convert("RGB")
+            image = np.array(json.loads(image)).astype(np.float32)
+            image = torch.from_numpy(image)[None,]
+            # if 'A' in i.getbands():
+            #     mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+            #     mask = 1. - torch.from_numpy(mask)
+            # else:
+            mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+            return (image, mask)
+        except:
+            image_path = folder_paths.get_annotated_filepath(image)
+            i = Image.open(image_path)
+            i = ImageOps.exif_transpose(i)
+            image = i.convert("RGB")
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+            if 'A' in i.getbands():
+                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            else:
+                mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+            return (image, mask)
 
     @classmethod
     def IS_CHANGED(s, image):
--- server.py
+++ server.py
@@ -10,6 +10,9 @@ import glob
 import struct
 from PIL import Image
 from io import BytesIO
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaRelay
+from rtc_utils import VideoTransformTrack
 
 try:
     import aiohttp
@@ -26,6 +29,8 @@ from comfy.cli_args import args
 import comfy.utils
 import comfy.model_management
 
+pcs = set()
+relay = MediaRelay()
 
 class BinaryEventTypes:
     PREVIEW_IMAGE = 1
@@ -83,6 +88,76 @@ class PromptServer():
         self.routes = routes
         self.last_node_id = None
         self.client_id = None
+
+        @routes.post("/offer")
+        async def offer(request):
+            params = await request.json()
+            offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+            pc = RTCPeerConnection()
+            pc_id = "PeerConnection(%s)" % uuid.uuid4()
+            pcs.add(pc)
+
+            def log_info(msg, *args):
+                print(pc_id + " " + msg, *args)
+
+            log_info("Created for %s", request.remote)
+
+            # # prepare local media
+            # player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
+            # if args.record_to:
+            #     recorder = MediaRecorder(args.record_to)
+            # else:
+            #     recorder = MediaBlackhole()
+
+            @pc.on("datachannel")
+            def on_datachannel(channel):
+                @channel.on("message")
+                def on_message(message):
+                    if isinstance(message, str) and message.startswith("ping"):
+                        channel.send("pong" + message[4:])
+
+            @pc.on("connectionstatechange")
+            async def on_connectionstatechange():
+                log_info("Connection state is %s", pc.connectionState)
+                if pc.connectionState == "failed":
+                    await pc.close()
+                    pcs.discard(pc)
+
+            @pc.on("track")
+            def on_track(track):
+                log_info("Track %s received", track.kind)
+
+                # if track.kind == "audio":
+                #     pc.addTrack(player.audio)
+                #     recorder.addTrack(track)
+                # elif track.kind == "video":
+                pc.addTrack(
+                    VideoTransformTrack(
+                        relay.subscribe(track)#, transform=params["video_transform"]
+                    )
+                )
+                    # if args.record_to:
+                    #     recorder.addTrack(relay.subscribe(track))
+
+                @track.on("ended")
+                async def on_ended():
+                    log_info("Track %s ended", track.kind)
+                    # await recorder.stop()
+
+            # handle offer
+            await pc.setRemoteDescription(offer)
+            # await recorder.start()
+
+            # send answer
+            answer = await pc.createAnswer()
+            await pc.setLocalDescription(answer)
+
+            print(f"sdp: {pc.localDescription.sdp}")
+
+            return web.json_response(
+                {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+            )
 
         @routes.get('/ws')
         async def websocket_handler(request):
@@ -411,16 +486,16 @@ class PromptServer():
 
             if "prompt" in json_data:
                 prompt = json_data["prompt"]
-                valid = execution.validate_prompt(prompt)
+                # valid = execution.validate_prompt(prompt)
                 extra_data = {}
                 if "extra_data" in json_data:
                     extra_data = json_data["extra_data"]
 
                 if "client_id" in json_data:
                     extra_data["client_id"] = json_data["client_id"]
-                if valid[0]:
+                if True: #valid[0]:
                     prompt_id = str(uuid.uuid4())
-                    outputs_to_execute = valid[2]
+                    outputs_to_execute = ["8"] #valid[2]
                     self.prompt_queue.put((number, prompt_id, prompt, extra_data, outputs_to_execute))
                     return web.json_response({"prompt_id": prompt_id, "number": number})
                 else:
@@ -510,6 +585,8 @@ class PromptServer():
     def send_sync(self, event, data, sid=None):
         self.loop.call_soon_threadsafe(
             self.messages.put_nowait, (event, data, sid))
+        self.loop.call_soon_threadsafe(
+            self.messages.put_nowait, (event, data, "1234"))
 
     def queue_updated(self):
         self.send_sync("status", { "status": self.get_queue_info() })
